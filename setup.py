0bbe234e3a39f111b6d8079eda60f7a597e256a7
Jeicex1 <99057897+Jeicex1@users.noreply.github.com>
2024-08-15 01:20:12 -0700

    pypi requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 setuptools.setup(
     name="wikiscraper",
     version="1.0.1",
-    author="Jeicex1",
+    author="Joseph Press",
     author_email="joepress101@gmail.com",
     description="scrape Wikipedia tables into CSV’s, enhancing data compatibility for text processing.",
     long_description=long_description,
@@ -19,6 +19,7 @@ setuptools.setup(
     scripts=["scripts/wikiscraper"],
     install_requires=requirements,
     classifiers=[
+        'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
