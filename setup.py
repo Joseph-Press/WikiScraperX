5060a951e430089a2f4c0076f9b4d78c326c12dd
Jeicex1 <99057897+Jeicex1@users.noreply.github.com>
2024-08-15 03:27:11 -0700

    fix requirements

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ else:
 
 setuptools.setup(
     name="wikiscraperx",
-    version="1.0.3",
+    version="1.0.4",
     author="Joseph Press",
     author_email="joepress101@gmail.com",
     description="scrape Wikipedia tables into CSV’s, enhancing data compatibility for text processing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Joseph-Press/wikiscraperx",
-    download_url="https://github.com/Joseph-Press/WikiScraperX/archive/refs/tags/1.0.3.tar.gz",
+    download_url="https://github.com/Joseph-Press/WikiScraperX/archive/refs/tags/1.0.4.tar.gz",
     packages=setuptools.find_packages(),
     scripts=["scripts/wikiscraperx"],
     install_requires=[
@@ -24,7 +24,6 @@ setuptools.setup(
           'beautifulsoup4',
           "lxml",
           "requests",
-          "table_parser"
       ],
     classifiers=[
         'Development Status :: 3 - Alpha',
--- wikiscraper/mainCli.py
+++ wikiscraper/mainCli.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-from table_parser import TableParser  
+from tableParser import TableParser  
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
